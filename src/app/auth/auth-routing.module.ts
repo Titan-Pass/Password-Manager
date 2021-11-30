b2668f4d22ae9c9287d9cb29f220f0546627a609
Christian Hussmann <chri.hussmann@gmail.com>
2021-11-30 12:11:23 +0100

    started forgot password

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
+import {ForgotPasswordComponent} from "./forgot-password/forgot-password.component";
 
 const routes: Routes = [{
-  path: 'login', component: LoginComponent
+  path: 'login', component: LoginComponent},
+  {path: 'forgotpassword', component: ForgotPasswordComponent
 }];
 
 @NgModule({
