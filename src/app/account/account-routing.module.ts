4fa9bb4988b14d49fe96577485ec1a138f5c2f45
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-12-02 12:16:23 +0100

    create routing

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AccountDashboardComponent} from "./dashboard/account.dashboard.component";
+import {CreateAccountComponent} from "./create/create.account.component";
 
 const routes: Routes = [
-  {path: 'dashboard', component: AccountDashboardComponent}
+  {path: 'dashboard', component: AccountDashboardComponent},
+  {path: 'create', component: CreateAccountComponent}
 ];
 
 @NgModule({
