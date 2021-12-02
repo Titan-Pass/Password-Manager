c9aac537bc64ee31b1ce95bfcf64ee6845877974
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-12-02 12:23:28 +0100

    Fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthInterceptor} from "./auth/interceptors/auth.interceptor";
-import {AccountDashboardComponent} from "./account/dashboard/account.dashboard.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountDashboardComponent
   ],
   imports: [
     BrowserModule,
